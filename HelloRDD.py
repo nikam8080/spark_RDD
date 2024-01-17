255918004c813d931121dca7d602c57efa265573
Nikhil Nikam <nikhilnikam@Nikhils-MBP-2.comnect.local>
2024-01-17 11:14:15 +0530

    first commit

---
+++ HelloRDD.py
@@ -0,0 +1,42 @@
+import sys
+from asyncio.log import logger
+from collections import namedtuple
+from dataclasses import replace
+from distutils.command.config import config
+from posixpath import split
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from lib.logger import Log4j
+
+SurveyRecord = namedtuple("SurveyRecord", ["Age", "Gender", "Country", "State"])
+if __name__ == "__main__":
+
+    conf = SparkConf() \
+        .setMaster("local[3]") \
+        .setAppName("HelloRDD")
+    # sc = SparkContext(conf=conf)
+
+    spark = SparkSession \
+        .builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    sc = spark.sparkContext
+    logger = Log4j(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename")
+        sys.exit(-1)
+
+    linesRDD = sc.textFile(sys.argv[1])
+    partitionedRDD = linesRDD.repartition(2)
+
+    colsRDD = partitionedRDD.map(lambda line: line.replace('"', '').split(","))
+    selectRDD = colsRDD.map(lambda cols: SurveyRecord(int(cols[1]), cols[2], cols[3], cols[4]))
+    filteredRDD = selectRDD.filter(lambda r: r.Age < 40)
+    kvRDD = filteredRDD.reduceByKey(lambda r: (r.country, 1))
+    countRDD = kvRDD.reduceByKey(lambda v1, v2: v1 + v2)
+
+    colsList = countRDD.collect()
+    for x in colsList:
+        logger.info(x)
